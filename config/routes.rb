1b252f741ce0250ecd8670f27ed15381fa3f6fe9
ChrisH81 <chrishurst1981@icloud.com>
2017-05-19 13:18:20 +0200

    Display List

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    root "dinosaurs#index"
+
+    get "dinosaurs" => "dinosaurs#index"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,4 @@
 dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
 dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+dino4 = Dinosaur.create!( { name: "T-Rex", age: 1 + rand(98), image_url: "http://islanublar.jurassicworld.com/media/dinosaurs/tyrannosaurus-rex/tyrannosaurus-rex-info-graphic.png"})
